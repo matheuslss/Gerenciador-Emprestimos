4ef5f510f70ceaeb164a11badabf168b8cdf4d47
Matheus Lucas <matheuslucas.ssilva@gmail.com>
2019-10-03 13:33:15 -0300

    add mongoose.connect in server.js

--- src/server.js
+++ src/server.js
@@ -1,7 +1,15 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const routes = require('./routes');
 
+
 const app = express();
+
+mongoose.connect('mongodb+srv://mlss:mlss@cluster0-czbs7.mongodb.net/projeto-emprestimos?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+  //os dois comandos acima removem uns avisos chatos
+})
 
 app.use(express.json());
 app.use(routes);
